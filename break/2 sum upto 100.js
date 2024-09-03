aed86eb536cfbd26efa792a6064f249d777c7f42
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2024-09-03 18:23:25 +0600

    break 2 done

--- break/2 sum upto 100.js
+++ break/2 sum upto 100.js
@@ -1,3 +1,17 @@
 /* 
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
+let num = 1;
+let sum = 0;
+
+while (true) {
+  sum += num;
+  console.log(`num`,num)
+  console.log(sum)
+  num++;
+  if (sum >= 100) {
+    break;
+  }
+}
+
+console.log(sum)
--- break/3 square break.js
+++ break/3 square break.js
@@ -1,3 +1,17 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+
+let num = 1;
+
+while (num <= 100) {
+  console.log(num);
+  // Calculate the square root of the current number
+  let sqrt = Math.sqrt(num);
+  
+  // Check if the square root is an integer
+  if (sqrt === Math.floor(sqrt)) {
+    continue;
+  }
+  num++;
+}
